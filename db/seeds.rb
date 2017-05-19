e7f357cc0158c744eff53f9463ffcd64f4e15e48
kylewelt <kylewelt@umich.edu>
2017-05-19 14:32:50 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student = Student.create(first_name: "Kyle", last_name: "Welton", created_at: Time.now, updated_at: Time.now)
